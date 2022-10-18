69c5db47c136f5d680827a49d9f8aa188c816365
Srivarshann <113925253+Srivarshann@users.noreply.github.com>
2022-10-18 15:23:26 +0530

    Update Assignment3.py

--- Assignments/team member2/Assignment3.py
+++ Assignments/team member2/Assignment3.py
@@ -5,13 +5,13 @@ from time import sleep
 
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW) 
 
 while True: 
- GPIO.output(8, GPIO.HIGH) 
- sleep(1) # Sleep for 1 second
- GPIO.output(8, GPIO.LOW) 
- sleep(1) # Sleep for 1 second
+ GPIO.output(6, GPIO.HIGH) 
+ sleep(10) # Sleep for 10 second
+ GPIO.output(6, GPIO.LOW) 
+ sleep(10) # Sleep for 10 second
 
 
 #traffic light program
@@ -22,28 +22,28 @@ from time import sleep
 
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
-GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
 while True: 
 
 #turn red light
 
- GPIO.output(8, GPIO.HIGH) 
- sleep(2) 
- GPIO.output(8, GPIO.LOW) 
- sleep(2) 
+ GPIO.output(9, GPIO.HIGH) 
+ sleep(7) 
+ GPIO.output(9, GPIO.LOW) 
+ sleep(7) 
 
 #turn yellow light
 
  GPIO.output(10, GPIO.HIGH) 
- sleep(2)
+ sleep(7)
  GPIO.output(10, GPIO.LOW) 
- sleep(2) 
+ sleep(7) 
 
 #turn green light
 
- GPIO.output(12, GPIO.HIGH)
- sleep(2) 
- GPIO.output(12, GPIO.LOW) 
- sleep(2)
+ GPIO.output(11, GPIO.HIGH)
+ sleep(7) 
+ GPIO.output(11, GPIO.LOW) 
+ sleep(7)
